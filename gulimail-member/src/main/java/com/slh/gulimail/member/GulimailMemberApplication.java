21edfd1c3cfa4cd4e9c149e1285a0844cff27e03
codeeLifer <1341687052@qq.com>
2021-07-17 11:17:19 +0800

    dev 项目基础环境配置---所有模块暂时完成，在启用spring cloud alibaba之前

--- gulimail-member/src/main/java/com/slh/gulimail/member/GulimailMemberApplication.java
+++ gulimail-member/src/main/java/com/slh/gulimail/member/GulimailMemberApplication.java
@@ -1,8 +1,10 @@
 package com.slh.gulimail.member;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.slh.gulimail.member.dao")
 @SpringBootApplication
 public class GulimailMemberApplication {
 
